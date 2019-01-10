47cd21629a5289c9333568a2ff87ac581bd44ae3
yaxuan zhang <yaxuanz66@gmail.com>
2019-01-10 17:41:40 +0800

    run this!
    run this!

---
+++ run.bat
@@ -0,0 +1,21 @@
+﻿@echo off
+chcp 65001
+@echo off
+set /p pname=-^> 请输入程序名称：
+echo.
+echo ============程序开始编译============
+echo.
+fpc ./%pname%.pas
+if errorlevel 1 (
+	echo 程序编译错误。不再执行程序。
+	pause
+	exit /b %errorlevel%
+)
+echo.
+echo ============程序开始执行============
+%pname%
+echo.
+echo ============程序执行完毕============
+echo.
+echo 按E键退出...
+pause>nul
